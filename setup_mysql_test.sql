14ef5a905d4fc91da448ef9c036f88b1e60b77b5
WangariNelly <nellywangari30@mail.com>
2022-09-14 15:38:49 +0300

    dev tests

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- script that prepares a MySQL server for the project
+-- have all privileges on the database
+-- have SELECT privilege on the database
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+USE hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
