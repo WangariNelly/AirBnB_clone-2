da422ea675ab1a13866215c653a605d1be784e2d
WangariNelly <nellywangari30@mail.com>
2022-09-14 15:53:47 +0300

    Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,12 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls:
+            return {key: obj for (key, obj) in self.__objects.items()
+                    if isinstance(obj, cls)}
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -34,6 +37,15 @@ class FileStorage:
         from models.city import City
         from models.amenity import Amenity
         from models.review import Review
+
+    def delete(self, obj=None):
+        """Deletes obj if it's inside the attribute __objects
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            if (key, obj) in self.__objects.items():
+                self.__objects.pop(key, None)
+        self.save()
 
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
